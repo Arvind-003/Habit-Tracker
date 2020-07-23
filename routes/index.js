1e4903dcec97dd7cfcc33a2e7d02260cab67a148
Arvind Kumar Mahto <arvind.kumar8271043313@gmail.com>
2020-07-23 11:48:01 +0530

    users.js in route created and users_controller is created , we have accecss user.js route from index.js(routes),and from users.js our control goes to users_controller

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,8 @@ console.log('router loaded');
 //accessing home controller action
 router.get('/',homeController.home);
 
+//use express router to go users.js
+router.use('/users',require("./users"));
+
 // i want this router to available in index.js main ,so i will export it and we will app to use it in main index.js
 module.exports=router;
