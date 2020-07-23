b2e874a028f5969f1aac5225e70ea1b929baf797
Arvind Kumar Mahto <arvind.kumar8271043313@gmail.com>
2020-07-23 11:26:35 +0530

    home_controller created and we aceess from routes index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@
 const express=require('express');
 
 const router=express.Router();
+const homeController=require('../controllers/home_controller');
 
 console.log('router loaded');
 
-
-
+//accessing home controller action
+router.get('/',homeController.home);
 
 // i want this router to available in index.js main ,so i will export it and we will app to use it in main index.js
 module.exports=router;
